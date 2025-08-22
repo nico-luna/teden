f2c95636f3688897281bbc788156d1f62d6756ed
Nicolas Ariel Luna <79757865+nico-luna@users.noreply.github.com>
2025-08-22 20:37:25 -0300

    agregamos render

--- teden/settings.py
+++ teden/settings.py
@@ -33,6 +33,7 @@ CSRF_TRUSTED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
     "https://teden.net",
+    "https://teden.onrender.com",
     "https://217.196.61.35",
     "https://*.ngrok-free.app",
 ]
